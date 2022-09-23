487894a887b3346960b6f058fa36c266d13afde9
alunoctg <alunoctg@fiern.local>
2022-09-23 08:44:56 -0300

    adcionando banco

--- salao_de_beleza/salaoapp/views.py
+++ salao_de_beleza/salaoapp/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from salaoapp.forms import UsersForm
+from salaoapp.models import Usuario
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{})
+	tabela = Usuario.objects.all()
+	return render(request,'home.html',{'usuario':tabela})
+
+def cadastro(request):
+	data = {}
+	data['form'] = UsersForm()
+	return render(request,'cadastro.html',data)
+
+def docad(request):
+	form = UsersForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('cadastro')
+
+def docad(request):
+	form = UsersForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('cadastro')
